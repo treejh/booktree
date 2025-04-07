d833fb456a77f48322fd724402f1059fb9a78c44
jihyun <jihyun3066@naver.com>
2025-04-07 17:09:25 +0900

    [ add ] backend 디렉토리 추가

---
+++ backend/src/main/java/com/blog/booktree/BookTreeApplication.java
@@ -0,0 +1,13 @@
+package com.blog.booktree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookTreeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookTreeApplication.class, args);
+	}
+
+}
