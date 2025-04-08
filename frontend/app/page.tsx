42d8127a6892b78bbc58020c30ca892bbf72963b
jihyun <jihyun3066@naver.com>
2025-04-08 11:34:43 +0900

    [ delete / add ] frontend 디렉토리 삭제 후 추가

--- frontend/app/page.tsx
+++
@@ -1,6 +0,0 @@
-export default function Home() {
-  console.log(process.env.NEXT_PUBLIC_AIP_URL);
-  
-  return <div>HOME</div>
-  
-}
---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,5 @@
+export default function Home() {
+  console.log(process.env.NEXT_PUBLIC_API_BASE_URL);
+
+  return <div>HOME</div>;
+}
