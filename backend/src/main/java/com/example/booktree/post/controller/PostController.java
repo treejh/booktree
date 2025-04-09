da5f6346a17e3101e4ddd268aac569318e0544b7
jihyun <jihyun3066@naver.com>
2025-04-09 20:42:23 +0900

    [ add ] : #18 controller CRUD 코드 생성

--- backend/src/main/java/com/example/booktree/post/controller/PostController.java
+++ backend/src/main/java/com/example/booktree/post/controller/PostController.java
@@ -28,5 +28,37 @@ import org.springframework.web.bind.annotation.RestController;
 public class PostController {
     private final PostService postService;
 
+
+    @PostMapping
+    public ResponseEntity postPost(@Valid @RequestBody PostRequestDto postRequestDto) {
+        PostResponseDto response = new PostResponseDto(postService.createPost(postRequestDto));
+        return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    // Read
+    //내 블로그는 하나밖에 없다는 가정하에 유저 아이디로 가져옴
+    @GetMapping
+    public ResponseEntity getPostByUserId(@Positive @RequestParam Long postId) {
+        PostResponseDto response = new PostResponseDto(postService.findPostByPostId(postId));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    // Update
+    @PatchMapping("/{postId}")
+    public ResponseEntity patchPost(@RequestBody PostRequestDto postRequestDto
+            , @PathVariable("postId") Long postId) {
+        PostResponseDto response = new PostResponseDto(postService.updatePost(postRequestDto,postId));
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // Delete
+    @DeleteMapping("/{userId}/{postId}")
+    public ResponseEntity deletePost(@PathVariable("userId")Long userId,
+                                     @PathVariable("postId") Long postId) {
+        postService.deletePost(userId,postId);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+    }
+
     
 }
