5c2cb2e42019857bd3dbcb23f11e434cf3124ec4
hs.o_k <hs.o_k@naver.com>
2025-04-09 11:39:56 +0900

    [feat]#5 : Blog,Post entity

---
+++ backend/src/main/java/com/example/booktree/blog/entity/Blog.java
@@ -0,0 +1,51 @@
+package com.example.booktree.blog.entity;
+
+
+import com.example.booktree.auditable.Auditable;
+import com.example.booktree.user.entity.User;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="blogs")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString
+public class Blog extends Auditable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", nullable = false)
+    private User user;
+
+    @NotBlank
+    @Column(length = 20, nullable = false)
+    private String name; // 블로그 이름
+
+    @Column(length = 255)
+    private String profile; // 소개글(공지사항)
+
+    @Column(length = 255)
+    private String notice; // 공지사항
+
+    @Column(name = "created_at", nullable = false)
+    private LocalDateTime createdAt; // 생성날짜
+
+    @Column(name = "modified_at", nullable = false)
+    private LocalDateTime modifiedAt; // 수정날짜
+
+}
