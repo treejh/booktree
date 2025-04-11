87252fdc3c0ffce6b292cce3cc8a0c50843b4c0b
jerry6475 <jerry6475@naver.com>
2025-04-11 18:49:24 +0900

    [ fix ] : #56 충돌 해결

--- backend/src/main/java/com/example/booktree/post/controller/PostController.java
+++ backend/src/main/java/com/example/booktree/post/controller/PostController.java
@@ -14,7 +14,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/post")
+@RequestMapping("/api/v1/posts")
 @RequiredArgsConstructor
 public class PostController {
 
