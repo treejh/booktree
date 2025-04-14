9129338a6645ab9284a316cada9d1772bb79cdfd
bagsunyoung <malang333@naver.com.github.com>
2025-04-15 00:39:20 +0900

    [ feat ] #77 : 게시글 Id로 해당 게시글 조회, 블로그 Id에 해당하는 게시글 목록 조회, 유저 Id에 해당하는 게시글 목록 조회

--- backend/src/main/java/com/example/booktree/config/SecurityConfigBookTree.java
+++ backend/src/main/java/com/example/booktree/config/SecurityConfigBookTree.java
@@ -42,9 +42,10 @@ public class SecurityConfigBookTree {
                                 "/v3/api-docs/**",
                                 "/h2-console/**",
                                 "/api/users"
+
                         ).permitAll()
                         .requestMatchers("/api/v1/posts/create", "/api/v1/posts/patch/**", "/api/v1/posts/delete/**", "/api/v1/likeposts/click/**").hasAnyAuthority("ROLE_USER", "ROLE_ADMIN")
-                        .requestMatchers("/api/v1/users/get", "/api/v1/users/create","/api/v1/users/login", "/api/v1/likeposts/**").permitAll()
+                        .requestMatchers("/api/v1/users/get", "/api/v1/users/create","/api/v1/users/login", "/api/v1/likeposts/**", "/api/v1/posts/get/**").permitAll()
                         .anyRequest().authenticated()
                 )// "/api/v1/posts"
                 .oauth2Login(
