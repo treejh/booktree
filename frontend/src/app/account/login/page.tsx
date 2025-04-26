119d42ddf30beed6d409fb2440c77397e4a4c27c
jihyun <jihyun3066@naver.com>
2025-04-27 01:45:39 +0900

    [ front ] : #252 회원가입 카카오 시작하기 버튼 수정

--- frontend/src/app/account/login/page.tsx
+++ frontend/src/app/account/login/page.tsx
@@ -117,7 +117,7 @@ export default function LoginPage() {
 
                             <Link
                                 href={`${socialLoginForKakaoUrl}?redirectUrl=${redirectUrlAfterSocialLogin}`}
-                                className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-yellow-400 hover:bg-yellow-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
+                                className="w-full flex justify-center items-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-gray-700 bg-[#FFE812] hover:bg-[#FFE200] focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-yellow-500"
                             >
                                 <svg viewBox="0 0 24 24" className="w-5 h-5 mr-2 fill-current">
                                     <path d="M12 3C6.5 3 2 6.5 2 11c0 2.5 1.2 4.7 3 6.2l-1 3.8 4-2.4c1.3.4 2.6.6 4 .6 5.5 0 10-3.5 10-8s-4.5-8-10-8z" />
