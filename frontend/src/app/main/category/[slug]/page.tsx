8bda52fd961ab97dee290de355cb91f8bbb5284f
jerry6475 <jerry6475@naver.com>
2025-04-27 18:34:04 +0900

    [ front ] : #273 메인 카테고리 별 게시글 가져오기 조회수, 날짜 프론트 연동

--- frontend/src/app/main/category/[slug]/page.tsx
+++ frontend/src/app/main/category/[slug]/page.tsx
@@ -60,7 +60,10 @@ const CategoryDetailPage = () => {
                     title: post.title,
                     // description: post.content, // 필요에 따라 추가
                     url: post.imageUrl, // 필요에 따라 추가
+                    createAt: post.createdAt,
+                    viewCount: post.viewCount,
                 }))
+                console.log('결과 : ', result)
 
                 setPosts(transformedPosts)
             })
@@ -157,7 +160,12 @@ const CategoryDetailPage = () => {
                                         </div>
                                         <div>
                                             <h3 className="text-lg font-medium mb-2">{post.title}</h3>
-                                            {/* <p className="text-gray-600 text-sm">{post.description}</p> */}
+                                            <br></br>
+                                            <p className="text-sm text-gray-500 mb-1">조회수: {post.viewCount}</p>
+                                            <p className="text-sm text-gray-500">
+                                                작성일:{' '}
+                                                {new Date(post.createAt).toLocaleDateString('ko-KR').replace(/\.$/, '')}
+                                            </p>
                                         </div>
                                     </div>
                                 </Link>
