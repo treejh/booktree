0261da69c593ced24f37d840a4794bfd0551d0b1
jihyun <jihyun3066@naver.com>
2025-04-28 14:48:55 +0900

    [ add ] : #311 next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -4,6 +4,11 @@ const nextConfig = {
   images: {
     domains: ["booktree-s3-bucket.s3.ap-northeast-2.amazonaws.com"],
   },
+
+  eslint: {
+    // 빌드 중에 ESLint 검사를 건너뜁니다
+    ignoreDuringBuilds: true,
+  },
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
