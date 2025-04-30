fa44fc7b95ee1215ab99f1b580f435646811623e
jihyun <jihyun3066@naver.com>
2025-04-30 11:18:24 +0900

    [ delete ] : test 코드 제거

--- backend/src/test/java/com/example/booktree/BookTreeApplicationTests.java
+++ backend/src/test/java/com/example/booktree/BookTreeApplicationTests.java
@@ -1,13 +1,13 @@
-package com.example.booktree;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class BookTreeApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.example.booktree;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class BookTreeApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
