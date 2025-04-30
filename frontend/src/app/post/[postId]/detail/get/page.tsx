ad73d397bcc74290089bab7ed6ddc2517d3ad834
jihyun <jihyun3066@naver.com>
2025-05-01 00:30:38 +0900

    [ refact ] : DDD 없앰

--- frontend/src/app/post/[postId]/detail/get/page.tsx
+++ frontend/src/app/post/[postId]/detail/get/page.tsx
@@ -1151,7 +1151,7 @@ export default function DetailPage() {
                                                     d="M4 16l4.586-4.586a2 2 0 012.828 0L16 16m-2-2l1.586-1.586a2 2 0 012.828 0L20 14m-6-6h.01M6 20h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"
                                                 />
                                             </svg>
-                                            이미지 첨부DDD
+                                            이미지 첨부
                                         </label>
                                     </div>
                                     <div
