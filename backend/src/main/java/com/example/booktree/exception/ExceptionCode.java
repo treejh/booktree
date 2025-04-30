1f17ecd72a198ca5f705bbf90e567368a3951feb
jihyun <jihyun3066@naver.com>
2025-04-30 11:21:50 +0900

    [ refact ] : #392 Exception 코드 개행 제거

--- backend/src/main/java/com/example/booktree/exception/ExceptionCode.java
+++ backend/src/main/java/com/example/booktree/exception/ExceptionCode.java
@@ -31,9 +31,7 @@ public enum ExceptionCode {
     USER_NOT_LOGGED_IN(403, "아직 로그인 상태가 아니어서 좋아요를 누를 권한이 없습니다."),
     BLOG_NOT_OWNER(403, "해당 블로그의 소유자가 아닙니다."),
     MAIN_CATEGORY_NOT_FOUND(404, "메인 카테고리가 존재하지 않습니다."),
-    INVALID_CONTENT_PARTS(404, "게시글의 내용 이미지와 글의 조합이 유효하지 않습니다.")
-
-    ;
+    INVALID_CONTENT_PARTS(404, "게시글의 내용 이미지와 글의 조합이 유효하지 않습니다.");
 
 
 
